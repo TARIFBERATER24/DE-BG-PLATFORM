4451c6b286242d7dd727ee6684780f7908f35a7c
Vilislav Gitsov <vilislavsvetlozarov@gmail.com>
2026-08-25 17:08:45 +0300

    Connect homepage header tools

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import NavDropdown from "@/components/NavDropdown";
 import SearchBar from "@/components/SearchBar";
 import MobileNav from "@/components/MobileNav";
+import HeaderTools from "@/components/HeaderTools";
 import { WhatsAppIcon } from "@/components/icons";
 
 const WHATSAPP_URL = "https://wa.me/message/JXXTA3JHKDX3L1";
@@ -44,6 +45,7 @@ export default function Header() {
             <WhatsAppIcon className="h-5 w-5" />
             Помощ
           </a>
+          <HeaderTools />
           <NavDropdown />
         </div>
 
@@ -56,3 +58,4 @@ export default function Header() {
     </header>
   );
 }
+
